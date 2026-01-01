08675b23d97a383240975e65e4a7abf3f6ff5209
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-01 23:59:19 +0530

    fix carousel for mobile

--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -41,37 +41,37 @@ export const Carousel = () => {
                     
                     <div 
                         key={slide.id}
-                        className="w-full h-full shrink-0 bg-gray-950 flex justify-between items-center px-16"
+                        className="w-full h-full shrink-0 bg-gray-950 flex flex-col justify-center items-center gap-10 "
                     >
-                        <div className="flex flex-col max-w-2xl">
-                            <h1 className="text-white text-6xl font-semibold">{slide.name}</h1>
-                            <p className="text-gray-300 text-left mt-6 line-clamp-2">
+                        <div className="flex flex-col justify-center items-center max-w-70 max-h-80  overflow-hidden">
+                            <h1 className="text-white text-2xl md:text-6xl font-semibold">{slide.name}</h1>
+                            <p className="text-gray-300 text-sm text-left mt-3 hidden md:mt-6 md:line-clamp-2">
                                 {slide.shortDescription}
                             </p>
                             
-                            <ul className="flex gap-4 mt-6">
+                            <ul className=" grid grid-cols-3 gap-3 mt-6">
                                 {slide.tags.map((tag, index) => (
-                                    <li key={index} className="border border-gray-500 text-white rounded-full px-3 py-1 text-xs tracking-wider">
+                                    <li key={index} className={`${index >= 3 ? "hidden sm:flex" : "flex"} justify-center items-center border border-gray-500 text-white  rounded-full px-2 py-1 md:px-3 md:py-1 text-xs md:text-xs md:tracking-wider`}>
                                         {tag}
                                     </li>
                                 ))}
                             </ul>
 
-                            <div className="flex gap-6 items-center mt-8">
+                            <div className="flex gap-6  justify-center items-center mt-8">
                                 <a href={slide.website} target="_blank" rel="noreferrer">
-                                    <button className="text-white bg-indigo-500 hover:bg-indigo-700 rounded-lg text-lg px-8 py-2 transition-all duration-200 hover:-translate-y-1">
+                                    <button className="text-white bg-indigo-500 hover:bg-indigo-700 rounded-lg text-sm px-4 py-1 md:text-lg md:px-8 md:py-2 transition-all duration-200 hover:-translate-y-1">
                                         Visit
                                     </button>
                                 </a>
-                                <a href={`/tool/${slide.id}`} className="text-gray-400 underline underline-offset-4 text-sm hover:text-white transition-colors">
+                                <a href={`/tool/${slide.id}`} className="text-gray-400 underline underline-offset-4 text-xs md:text-sm hover:text-white transition-colors">
                                     Get Details
                                 </a>
                             </div>
                         </div>
 
                         
-                        <div className={`flex justify-center items-center w-50 h-50 rounded-2xl shrink-0 ${slide.logotype === "white" ? "bg-black" : "bg-white"}`}>
-                            <img src={slide.logo} alt={slide.name} className="h-40 w-auto object-contain" />
+                        <div className={`-order-1 flex justify-center items-center max-w-20 max-h-20 rounded-2xl  shrink-0 ${slide.logotype === "white" ? "bg-black" : "bg-white"}`}>
+                            <img src={slide.logo} alt={slide.name} className=" w-full h-20 object-contain" />
                         </div>
                     </div>
                 ))}
