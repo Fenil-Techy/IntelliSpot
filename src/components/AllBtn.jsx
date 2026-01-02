2e775d044e8c38ceda24b40cd0ad1726a08ab602
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-02 23:31:09 +0530

    all button add

---
+++ src/components/AllBtn.jsx
@@ -0,0 +1,6 @@
+export const AllBtn = () => {
+    return (
+
+        <button className=" mt-6 flex m-auto text-sm px-3 py-2  md:px-5 md:py-4 text-white rounded-lg md:text-md bg-indigo-500 transition-all hover:-translate-y-1 duration-100 hover:shadow-md hover:bg-indigo-600">Explore All Tools</button>
+    )
+}
