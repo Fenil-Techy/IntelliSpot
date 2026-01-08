1a9b2886d957820190ae1b4c813fe97aafc85fde
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-09 02:05:04 +0530

    extract load more

--- src/features/home/sections/usecase/PopularUsecase.jsx
+++ src/features/home/sections/usecase/PopularUsecase.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdExpandMore } from "react-icons/md";
+import { LoadMore } from "../../../../shared/component/Loadmore";
 
 
 export const PopularUsecase = () => {
@@ -66,18 +67,7 @@ export const PopularUsecase = () => {
                 </div>
 
                 {/* Load More Button */}
-                <div className="mt-8 flex justify-center">
-                    <button
-                        onClick={() => setIsExpanded(!isExpanded)}
-                        className="flex items-center gap-2 text-sm text-muted-foreground hover:text-white transition"
-                    >
-                        {isExpanded ? "Show less" : "Load more"}
-                        <MdExpandMore
-                            className={`h-4 w-4 transition-transform ${isExpanded ? "rotate-180" : ""
-                                }`}
-                        />
-                    </button>
-                </div>
+                <LoadMore isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
             </div>
 
 
