0da7699b0d537e4b27a7a636df98cdb629a45cb6
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-13 22:48:21 +0530

    improve tool page

--- src/features/Tools/Tools.jsx
+++ src/features/Tools/Tools.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import { CategoryList } from "./section/CategoryList"
 import { SearchBar } from "./section/SearchBar"
+import { ToolList } from "./section/ToolList"
+
 export const Tools=()=>{
+    const [selectedCategory, setSelectedCategory] = useState("All")
+    const [searchQuery, setSearchQuery] = useState("")
+    
     return (
         <div className="p-4">
-            <CategoryList/>
-            <SearchBar/>
+            <CategoryList selectedCategory={selectedCategory} onCategoryChange={setSelectedCategory}/>
+            <SearchBar searchQuery={searchQuery} onSearchChange={setSearchQuery}/>
+            <ToolList selectedCategory={selectedCategory} searchQuery={searchQuery}/>
         </div>
     )
 }
