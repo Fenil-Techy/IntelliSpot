e63cac49da68a2dfff329e45803e0ea9e614f4ad
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-01-13 22:48:05 +0530

    add search feature

--- src/features/Tools/section/SearchBar.jsx
+++ src/features/Tools/section/SearchBar.jsx
@@ -1,9 +1,11 @@
-export const SearchBar = () => {
+export const SearchBar = ({ searchQuery, onSearchChange }) => {
   return (
     <div className="py-10 flex items-center max-w-md mx-auto">
       <input
         type="text"
         placeholder="Search AI Tool"
+        value={searchQuery}
+        onChange={(e) => onSearchChange(e.target.value)}
         className="w-full h-10 px-4 border border-gray-300 rounded-l-full focus:outline-none focus:ring-1 focus:ring-gray-800"
       />
       <button className="h-10 px-4 bg-gray-800 text-white rounded-r-full hover:bg-gray-700">
